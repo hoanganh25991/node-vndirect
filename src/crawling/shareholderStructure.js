e8577df6342e1ee234c16e50320fe79d4e70a5c7
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-11-25 15:12:53 +0000

    Basic shareholder structure

---
+++ src/crawling/shareholderStructure.js
@@ -0,0 +1,62 @@
+import readDescription from "../utils/page/readDescription"
+import { removeSymbolOnCharacter } from "../utils/removeSymbolOnCharacter"
+
+const getDescription = url => {
+  return [
+    {
+      title: `Go to url: ${url}`,
+      goto: url
+    },
+    {
+      title: `Expose remove symbol in characters`,
+      exposeFunction: ["removeSymbol", removeSymbolOnCharacter]
+    },
+    {
+      title: `Run evaluate`,
+      evaluate: async () => {
+        try {
+          const liArr = [...document.querySelectorAll("ul.box_cocau_codong li")]
+
+          let data = {}
+          let transVn = {}
+
+          const dataArr = await Promise.all(
+            liArr.map(async li => {
+              const title = li.querySelector(":nth-child(1)").innerText
+              const value = li.querySelector(":nth-child(2)").innerText
+              const key = await window.removeSymbol(title)
+              return { key, title, value }
+            })
+          )
+
+          dataArr.forEach(obj => {
+            const { title, key, value } = obj
+            data[key] = value
+            transVn[key] = title
+          })
+
+          return { data, transVn }
+        } catch (err) {
+          return "HasErr"
+        }
+      },
+      storeReturnAsKey: "shareholderStructure"
+    }
+  ]
+}
+
+/**
+ * Crawling categories
+ * @param getState
+ * @param dispatch
+ * @constructor
+ */
+export const crawlingShareholderStructure = (getState, dispatch) => async url => {
+  dispatch({ type: "LOG", msg: `\x1b[36m<<< GET OWNERSHIP >>>\x1b[0m` })
+
+  const storeReturn = await readDescription(getState, dispatch)(getDescription(url))
+  const { shareholderStructure: { data, transVn } } = storeReturn
+  return { data, transVn }
+}
+
+export default crawlingShareholderStructure
---
+++ src/crawling/shareholderStructure.test.js
@@ -0,0 +1,34 @@
+import { crawlingShareholderStructure } from "./shareholderStructure"
+import { combineReducers, createStore } from "redux"
+import { logReducers, LogToConsole } from "../reducers/logReducers"
+import { TinyPage } from "../utils/page/TinyPage"
+
+// Setup store
+const store = createStore(combineReducers({ logState: logReducers }))
+LogToConsole(() => {
+  const { logState } = store.getState()
+  return logState
+}, store)
+
+const { dispatch } = store
+
+// Stuff
+const _ = console.log
+
+// Start test
+const TEST_CASE = "Crawling Ownership"
+let pass = true
+;(async () => {
+  const url = "https://www.vndirect.com.vn/portal/co-dong-chinh/aam.shtml"
+  try {
+    const { data, transVn } = await crawlingShareholderStructure(null, dispatch)(url)
+    _("Recheck", data)
+    pass = data && transVn
+  } catch (err) {
+    _(err)
+    pass = false
+  } finally {
+    await TinyPage.closeBrowser()
+    pass ? _(`\x1b[42m[PASS]\x1b[0m ${TEST_CASE}`) : _(`\x1b[41m[FAIL]\x1b[0m ${TEST_CASE}`)
+  }
+})()
