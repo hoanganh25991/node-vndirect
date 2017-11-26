78cc8c842cd128d0d309beef94a7eaeaf5d3797d
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-11-26 10:03:18 +0700

    Add warning

--- src/crawling/balanceSheet.js
+++ src/crawling/balanceSheet.js
@@ -74,12 +74,15 @@ const getDescriptionOneYear = (url, year) => {
         const parseQuater = (str, tz = 7) => {
           const matcheds = str.match(/(\d+)\/(\d+)/)
           const qX = +matcheds[1]
-          const yearX = matcheds[2]
+          const yrX = matcheds[2]
           // 2008-09-15T15:53:00+0500
           const month = (qX - 1) * 3 + 1
           // I want the default timezone is Asia/Ho_Chi_Minh
           const tzStr = `${tz}`.length === 2 ? `${tz}` : `0${tz}`
-          const date = new Date(`${yearX}-${month}-01T00:00:00+${tzStr}00`)
+          const mSr = `${month}`.length === 2 ? `${month}` : `0${month}`
+
+          const date = new Date(`${yrX}-${mSr}-01T00:00:00+${tzStr}00`)
+
           return Math.floor(date.getTime() / 1000)
         }
 
--- src/utils/parseQuater.test.js
+++ src/utils/parseQuater.test.js
@@ -13,6 +13,7 @@ _("RECHECK", quaterTs)
 pass = pass && quaterTs === expectedTs
 
 // Test case 2
+// When quater as 2 > month only 1 digit > err
 const str2 = "Q2/2017"
 const expectedTs2 = 1490979600
 const quaterTs2 = parseQuater(str2)
@@ -20,4 +21,5 @@ const quaterTs2 = parseQuater(str2)
 _("RECHECK", quaterTs2)
 pass = pass && quaterTs2 === expectedTs2
 
-pass ? _(`\x1b[42m[PASS]\x1b[0m ${TEST_CASE}`) : _(`\x1b[41m[FAIL]\x1b[0m ${TEST_CASE}`)
+_(`\x1b[31m Please manually UPATE page evaluate sandbox use this parse func \x1b[0m`)
+pass ? _(`\x1b[42m [PASS] \x1b[0m ${TEST_CASE}`) : _(`\x1b[41m [FAIL] \x1b[0m ${TEST_CASE}`)
