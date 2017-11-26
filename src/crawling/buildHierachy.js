89555d9ba02b53bb35a8308f57580950aed1446a
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-11-26 22:49:13 +0700

    Test buildHierachy when NO ROOT start at 0

--- src/crawling/buildHierachy.js
+++ src/crawling/buildHierachy.js
@@ -2,19 +2,19 @@ const _ = console.log
 
 export const buildHierachy = arr => {
   const root = { subs: [] }
+  const arrRootLv = arr[0].lv
   const parrentSheet = { root }
   arr.forEach(item => {
     const curItem = { ...item }
-    const { lv } = curItem
-
+    const { lv: itemLv } = curItem
+    const lv = itemLv - arrRootLv
     // Find his parent
     const pLv = lv > 0 ? lv - 1 : "root"
     const pItem = parrentSheet[pLv]
     const { subs = [] } = pItem
+    // Add him into his parent
     subs.push(curItem)
     pItem.subs = subs
-
-    // Add him into his parent
     // Add him into parentSheet
     parrentSheet[lv] = curItem
   })
--- src/crawling/buildHierachy.test.js
+++ src/crawling/buildHierachy.test.js
@@ -1,10 +1,12 @@
 import { buildHierachy } from "./buildHierachy"
 import testArr from "./buildHierachy.test.json"
+import testArr2 from "./buildHierachy2.test.json"
 
 const TEST_CASE = "Build Hierachy"
 const _ = console.log
 let pass = true
 
+// Test case 1
 const check = shape => {
   const expectdKey = "Cackhoanphaithunganhan"
   const item = shape[0].subs[0].subs[2]
@@ -12,6 +14,16 @@ const check = shape => {
 }
 
 const shape = buildHierachy(testArr)
-pass = check(shape)
+pass = pass && check(shape)
+
+// Test case 2
+const check2 = shape => {
+  const expectdKey = "Thunhapthuantuhoatdongdichvu"
+  const item = shape[5]
+  return expectdKey === item.key
+}
+
+const shape2 = buildHierachy(testArr2)
+pass = pass && check2(shape2)
 
 pass ? _(`\x1b[42m[PASS]\x1b[0m ${TEST_CASE}`) : _(`\x1b[41m[FAIL]\x1b[0m ${TEST_CASE}`)
